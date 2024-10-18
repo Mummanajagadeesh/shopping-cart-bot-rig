2e60e84cb0f08f9ce8b188669dcb7effa0ed44ac
M_Jagadeesh97 <mummanajagadeesh97@gmail.com>
2024-10-18 16:46:01 +0530

    Create upi_qr_code_gen.py

---
+++ CV/upi_qr_code_gen.py
@@ -0,0 +1,15 @@
+import qrcode
+import cv2
+import matplotlib.pyplot as plt
+
+price = 32
+img = qrcode.make(f'upi://pay?pa=7569664234@ybl&pn=JAGADEESH&am={price}&cu=INR')
+# upi://pay?pa=<UPI_ID>&pn=<Name>&mc=<Merchant_Code>&tid=<Transaction_ID>&tr=<Transaction_Ref_ID>&tn=<Transaction_Note>&am=<Amount>&cu=<Currency>&url=<URL>
+
+img.save('qrcode_test.png')
+image = cv2.imread("qrcode_test.png")
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
+
